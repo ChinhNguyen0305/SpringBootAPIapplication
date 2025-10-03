78b8230dc4eb63f78055ef3d2acb9ca9c4b7432b
SpringBootApp <giachinh0305@gmail.com>
2025-10-03 21:21:52 +0700

    First Commit

---
+++ src/test/java/chinh/nguyen/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package chinh.nguyen.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
